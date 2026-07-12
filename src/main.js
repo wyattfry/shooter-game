fc4f1cf36e1a4b7fd486534d0728bba6f07bd2cd
Wyatt Fry <wyattfry@gmail.com>
2026-07-11 20:03:14 -0400

    scale

--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 800,
+    height: 600
+  },
   physics: {
     default: 'arcade',
     arcade: {
